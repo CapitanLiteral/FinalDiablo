9c7a74453219ef2fd529be126287e82f13149353
dibu13 <dibu.13@gmail.com>
2016-05-12 03:45:07 +0200

    Entity movement & states :metal:

--- Motor2D/Entity.cpp
+++ Motor2D/Entity.cpp
@@ -7,6 +7,7 @@
 #include "EntityManager.h"
 #include "Player.h"
 #include "Attributes.h"
+#include "Pathfinding.h"
 
 Entity::Entity()
 {
@@ -20,6 +21,9 @@ Entity::~Entity()
 bool Entity::entityUpdate(float internDT)
 {
 	bool ret = true;
+
+	handleInput();
+	updateAction();
 
 	if (currentState != E_DEATH)
 	{
@@ -142,7 +146,14 @@ void Entity::draw()
 
 void Entity::drawDebug()
 {
+	app->render->DrawCircle(worldPosition.x, worldPosition.y, targetRadius, 255, 0, 0, 255, true);
+	app->render->DrawCircle(worldPosition.x, worldPosition.y, visionRadius, 0, 0, 255, 255, true);
 
+	if (movement)
+	{
+		app->render->DrawLine(worldPosition.x, worldPosition.y, target.x, target.y, 0, 0, 255);
+		app->render->DrawLine(worldPosition.x, worldPosition.y, velocity.x + worldPosition.x, velocity.y + worldPosition.y, 0, 255, 255);
+	}
 }
 
 
@@ -151,39 +162,104 @@ vector<iPoint> Entity::getNewPath(iPoint target)
 {
 	vector<iPoint> ret;
 
+	iPoint start = app->map->WorldToMap(worldPosition.x, worldPosition.y);
+	iPoint goal = target;
+	vector<iPoint> _path;
+	int steps = app->pathfinding->getNewPath(start, goal, path);
+	_path = path;
+	if (steps > 0)
+	{
+		//StateMachine change
+		current_input = EI_WALK;
+
+		movement = true;
+		currentNode = -1;
+		getNewTarget();
+	}
+
 	return ret;
 }
 
 void Entity::getNewTarget()
 {
-
+	if ((uint)currentNode + 1 < path.size())
+	{
+		currentNode++;
+		setTarget(app->map->getTileCenter(path[currentNode].x, path[currentNode].y));
+	}
+	else
+	{
+		//Maybe ERROR, watch out //setInput(INPUT_STOP_MOVE);
+		movement = false;
+	}
 }
 
-void Entity::setTarget(iPoint target)
+void Entity::setTarget(iPoint _target)
 {
-
+	target = _target;
+	movement = true;
+	targetReached = false;
 }
 
 void Entity::updateVelocity(float dt)
 {
+	velocity.x = target.x - worldPosition.x;
+	velocity.y = target.y - worldPosition.y;
 
+	velocity.SetModule(attributes->getMovementSpeed());
+
+	//Maybe ERROR, watch out //SetDirection();
 }
 
 void Entity::updateMovement(float dt)
 {
-	
+	if (movement)
+	{
+		if (!targetReached)
+		{
+			updateVelocity(dt);
+			move(dt);
+			targetReached = isTargetReached();
+		}
+		else
+		{
+			getNewTarget();
+		}
+	}
+	if (isInDestiny() || (player != NULL && player->getMapPosition().DistanceTo(worldPosition) <= targetRadius && currentState == E_WALK))
+	{
+		current_input = EI_STOP;
+	}
 }
 
 bool Entity::isTargetReached()
 {
-	bool ret = true;
+	fPoint vel;
 
-	return ret;
+	vel.x = target.x - worldPosition.x;
+	vel.y = target.y - worldPosition.y;
+
+	if (vel.getModule() <= targetRadius)
+	{
+		return true;
+	}
+
+	return false;
 }
 
 void Entity::move(float dt)
 {
+	fPoint vel = velocity * dt;
 
+	worldPosition.x += int(vel.x);
+	worldPosition.y += int(vel.y);
+
+	//NOTE: Collider movement, may be changed
+	collider->SetPos(worldPosition.x, worldPosition.y); //Maybe ERROR, watch out
+	if (isInDestiny() || (player != NULL && player->getMapPosition().DistanceTo(worldPosition) <= targetRadius && currentState == E_WALK))
+	{
+		path.clear();
+	}
 }
 
 void Entity::setMovement(int x, int y)
@@ -193,29 +269,203 @@ void Entity::setMovement(int x, int y)
 
 bool Entity::isInDestiny()
 {
-	bool ret = false;
+	if (app->map->WorldToMap(player->worldPosition.x, player->worldPosition.y) == app->map->WorldToMap(worldPosition.x, worldPosition.y))
+	{
+		return true;
+	}
 
-	return ret;
+	return false;
 }
 
 void Entity::setDirection()
 {
+	float angle = velocity.getAngle();
 
+	entityDirection dir;
+
+	if (angle < 22.5 && angle > -22.5)
+		dir = E_RIGHT;
+	else if (angle >= 22.5 && angle <= 67.5)
+		dir = E_UP_RIGHT;
+	else if (angle > 67.5 && angle < 112.5)
+		dir = E_UP;
+	else if (angle >= 112.5 && angle <= 157.5)
+		dir = E_UP_LEFT;
+	else if (angle > 157.5 || angle < -157.5)
+		dir = E_LEFT;
+	else if (angle >= -157.5 && angle <= -112.5)
+		dir = E_DOWN_LEFT;
+	else if (angle > -112.5 && angle < -67.5)
+		dir = E_DOWN;
+	else if (angle >= -67.5 && angle <= -22.5)
+		dir = E_DOWN_RIGHT;
+
+	if (dir != currentDirection)
+	{
+		currentDirection = dir;
+	}
 }
 
 void Entity::setDirection(fPoint pos)
 {
+	//NOTE: This has been created to change the direction without moving the player
+	fPoint direction;
+	direction.x = pos.x - worldPosition.x;
+	direction.y = pos.y - worldPosition.y;
 
+	direction.SetModule(1);
+
+	float angle = direction.getAngle();
+
+	entityDirection dir;
+
+	if (angle < 22.5 && angle > -22.5)
+		dir = E_RIGHT;
+	else if (angle >= 22.5 && angle <= 67.5)
+		dir = E_UP_RIGHT;
+	else if (angle > 67.5 && angle < 112.5)
+		dir = E_UP;
+	else if (angle >= 112.5 && angle <= 157.5)
+		dir = E_UP_LEFT;
+	else if (angle > 157.5 || angle < -157.5)
+		dir = E_LEFT;
+	else if (angle >= -157.5 && angle <= -112.5)
+		dir = E_DOWN_LEFT;
+	else if (angle > -112.5 && angle < -67.5)
+		dir = E_DOWN;
+	else if (angle >= -67.5 && angle <= -22.5)
+		dir = E_DOWN_RIGHT;
+
+	if (dir != currentDirection)
+	{
+		currentDirection = dir;
+	}
 }
 
 entityState Entity::updateAction()
 {
+	if (current_input != EI_NULL)
+	{
+		switch (currentState)
+		{
+		case E_IDLE:
+		{
+			if (current_input == EI_WALK)
+			{
+				currentState = E_WALK;
+			}
+			else if (current_input == EI_DIE)
+			{
+				currentState = E_DEATH;
+			}
+		}
+		break;
+
+		case E_WALK:
+		{
+			if (current_input == EI_STOP)
+			{
+				currentState = E_IDLE;
+			}
+			else if (current_input == EI_DIE)
+			{
+				currentState = E_DEATH;
+			}
+			else if (current_input == EI_ATTACK)
+			{
+				currentState = E_BASIC_ATTACK;
+			}
+		}
+		break;
+
+		case E_BASIC_ATTACK:
+		{
+			if (current_input == EI_STOP)
+			{
+				currentState = E_IDLE;
+			}
+			else if (current_input == EI_DIE)
+			{
+				currentState = E_DEATH;
+			}
+		}
+
+		break;
+
+		case E_DEATH:
+		{
+			if (current_input == EI_STOP)
+			{
+				currentState = E_IDLE;
+			}
+		}
+
+		break;
+
+		case RUNNING:
+		{
+			if (current_input == EI_STOP)
+			{
+				currentState = E_IDLE;
+			}
+			else if (current_input == EI_WALK)
+			{
+				currentState = E_WALK;
+			}
+			else if (current_input == EI_DIE)
+			{
+				currentState = E_DEATH;
+			}
+			else if (current_input == EI_ATTACK)
+			{
+				currentState = E_BASIC_ATTACK;
+			}
+		}
+		break;
+		}
+
+		if (previousState != currentState)
+		{
+
+		}
+
+		previousState = currentState;
+
+	}
+	current_input = EI_NULL;
+
 	return currentState;
 }
 
 void Entity::handleInput()
 {
-
+	if (!inputBlocked)
+	{
+		if (currentState != E_DEATH)
+		{
+			if (attributes->getLife() <= 0)
+			{
+				current_input = EI_DIE;
+			}
+			//Do things
+			if (player)
+			{
+				if (worldPosition.DistanceNoSqrt(player->getWorldPosition()) <= visionRadius * visionRadius)//Range vision
+				{
+					//comprobar si estas a rang d'atac
+					if (worldPosition.DistanceNoSqrt(player->getWorldPosition()) < targetRadius * targetRadius)
+					{
+						current_input = EI_ATTACK;
+					}
+					else
+					{
+						getNewPath(target);
+						current_input = EI_WALK;
+					}
+				}
+			}
+		}
+	}
 }
 
 
