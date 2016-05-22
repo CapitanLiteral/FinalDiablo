81f4d60fbf3b20477d723faa8f3cfdd73bde56e0
Josef21296 <jcasanovasv@hotmail.com>
2016-05-22 02:45:43 +0200

    Random enemy(still may not work cause must try walkability map yet)

--- Motor2D/Wolf.h
+++ Motor2D/Wolf.h
@@ -2,6 +2,7 @@
 #define __WOLF_H__
 
 #include "Entity.h"
+#include "Timer.h"
 
 class Wolf : public Entity
 {
@@ -9,7 +10,24 @@ public:
 	Wolf(iPoint pos);
 	~Wolf();
 
+	bool entityUpdate(float dt);
+
 private:
+	entityState updateAction();
+	void handleInput();
+
+	//Movement
+	void setInitVelocity();
+	void move(float dt);
+	void updateVelocity(float dt);
+
+	void updateMovement(float dt);
+
+	bool isInWalkableTile(iPoint coords);
+
+	Timer timerChangeDir;
+	uint32 changeDir = 1;
+
 };
 
 #endif
